f252a9b81def37845f7782206367dbc1811dc59e
Valerii Radchenko <valeravivaz@gmail.com>
2021-08-05 14:29:27 +0300

    format code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,69 +1,65 @@
-class RangeValidator{
-  constructor(from, to){
-    if(typeof (from && to) !== 'number'){
-      return new TypeError('value of "to" and "from" must be "string" type')
+class RangeValidator {
+  constructor(from, to) {
+    if (typeof (from && to) !== "number") {
+      return new TypeError('value of "to" and "from" must be "string" type');
     }
-    if(from < to){
+    if (from < to) {
       this._from = from;
       this._to = to;
     } else {
-      return new RangeError('value of "to" must be bigger, then "from"')
+      return new RangeError('value of "to" must be bigger, then "from"');
     }
 
     this.rangeArray = this.getterRange();
   }
 
-  set from(newFrom){
-    if(typeof newFrom !== 'number'){
-      return new TypeError('value of "from" must be "string" type')
+  set from(newFrom) {
+    if (typeof newFrom !== "number" || isNaN(newFrom)) {
+      return new TypeError('value of "from" must be "string" type');
     }
-    if(newFrom > this._to){
-      throw new RangeError('value of "to" must be bigger, then "from"')
+    if (newFrom > this._to) {
+      throw new RangeError('value of "to" must be bigger, then "from"');
     } else {
-      this._from = newFrom
+      this._from = newFrom;
     }
-    return this._from
-  }
-
-  get from(){
     return this._from;
   }
 
-  set to(newTo){
-    if(typeof newTo !== 'number'){
-      return new TypeError('value of "from" must be "string" type')
-    }
-    if(newTo < this._from){
-      throw new RangeError('value of "to" must be bigger, then "from"')
-    } else {
-      this._to = newTo
-    }
-    return this._to
+  get from() {
+    return this._from;
   }
 
-  get to(){
+  set to(newTo) {
+    if (typeof newTo !== "number" || isNaN(newTo)) {
+      return new TypeError('value of "from" must be "string" type');
+    }
+    if (newTo < this._from) {
+      throw new RangeError('value of "to" must be bigger, then "from"');
+    } else {
+      this._to = newTo;
+    }
     return this._to;
   }
 
+  get to() {
+    return this._to;
+  }
 
-  getterRange(){
+  getterRange() {
     const array = [];
-    for(let i=0; i<= this._to - this._from; i++){
-      array[i] = i + this._from; 
+    for (let i = 0; i <= this._to - this._from; i++) {
+      array[i] = i + this._from;
     }
     return array;
   }
 
-  
-  validate(value){
-    if(this.rangeArray.some(array => value === array)){
+  validate(value) {
+    if (this.rangeArray.some((array) => value === array)) {
       return value;
     } else {
-      return new RangeError('value is not been part of range');
+      return new RangeError("value is not been part of range");
     }
   }
-  
-
 }
 
-const range1 = new RangeValidator(1, 10)
+const range1 = new RangeValidator(1, 10);
